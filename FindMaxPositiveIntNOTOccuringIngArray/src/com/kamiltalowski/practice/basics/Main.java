43d489753709196f96d0e12d55dfe21dccf57f12
KamilTalowski <44585817+KamilTalowski@users.noreply.github.com>
2022-12-20 18:35:35 +0100

    UpdateComments

--- FindMaxPositiveIntNOTOccuringIngArray/src/com/kamiltalowski/practice/basics/Main.java
+++ FindMaxPositiveIntNOTOccuringIngArray/src/com/kamiltalowski/practice/basics/Main.java
@@ -1,14 +1,17 @@
 package com.kamiltalowski.practice.basics;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] A = {-5,-100,-2,-2,2,1, 3, 6, 4, 1, 2, 200};
+        int[] A = {-5,-100,-2,-2,2,1, 3, 6,4, 4, 1, 2, 200};
+
+
         int tempInt = 1;
-        int j=1;
+
+        //with sorting
+        //int j=1;
 /*
         Arrays.sort(A);
 
@@ -23,10 +26,12 @@ public class Main {
             }
         }
 */
+        //without sorting
         for(int i=1; i<A.length; i++){
                 for(int k=1; k<A.length; k++){
                     if (A[k]==tempInt) {
                         tempInt++;
+                        break;
                     }
                 }
 
